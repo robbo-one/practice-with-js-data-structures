bde5cfcac3cddde34b07af0fa399bf327ed630b5
robbo-one <73087376+robbo-one@users.noreply.github.com>
2021-01-06 11:44:17 +1300

    tests complete

--- getPropTypes.js
+++ getPropTypes.js
@@ -1,2 +1,9 @@
-function getPropTypes (obj) {
+function getPropTypes (obj) { 
+    let array = Object.values(obj)
+    return array.map((value)=> {
+        return typeof value
+    })
+    
 }
+
+module.exports = getPropTypes
