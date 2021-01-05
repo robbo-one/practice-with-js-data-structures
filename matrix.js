db156ace715c05f0a0a89d114559dcee2027e358
Ste-Perry <73620112+Ste-Perry@users.noreply.github.com>
2021-01-06 12:24:34 +1300

    The Matrix

--- matrix.js
+++ matrix.js
@@ -1,5 +1,29 @@
 function getMatrix (n) {
+  // console.log (n)
+    // console.log(math.matrix([n, n]))
+    //return math.zeros(n, n)
+    let matrix = new Array(n)
+    for (i = 0; i < matrix.length; i++){
+        matrix[i] = new Array(n)
+        matrix[i].fill(0)
+
+    }
+    return matrix
+    console.log(matrix)
+    
 }
 
 function updateMatrix (matrix, coords, value) {
+    // console.log(coords, value)
+  //  return matrix.map((value) => {
+   // return 
+   // })
+
+   matrix[coords[0]][coords[1]] = value
+   return matrix
+}
+
+module.exports = {
+    getMatrix: getMatrix,
+    updateMatrix: updateMatrix
 }
