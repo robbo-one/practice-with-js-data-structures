e39300667121c74ed3f978e8f070233b67c175c8
Ste-Perry <73620112+Ste-Perry@users.noreply.github.com>
2021-01-06 11:27:03 +1300

    getAddress

--- getAddress.js
+++ getAddress.js
@@ -1,2 +1,13 @@
 function getAddress (obj) {
+    // console.log (typeof obj)
+    // return obj.map(contact => {
+        return obj.address 
+    // })
+    console.log (obj) 
+  //  return Object.values(
+// obj.address
+//     )        
+
 }
+
+module.exports = getAddress
