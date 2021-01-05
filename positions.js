05902d1fcb900211da1a4bcde6c600d9680d27b2
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-01-06 11:32:26 +1300

    postions passed

--- positions.js
+++ positions.js
@@ -1,5 +1,14 @@
 function getFirst (arr) {
+    console.log(arr)
+    return arr[0]
 }
 
 function getLast (arr) {
+    console.log(arr)
+    return arr[arr.length - 1]
+}
+
+module.exports = {
+    getFirst: getFirst,
+    getLast: getLast
 }
