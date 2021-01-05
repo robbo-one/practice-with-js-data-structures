039c29ecf9265f8db463e91277b1a50d741ff3d3
Jack Bennett <73858124+j-ack-bennett@users.noreply.github.com>
2021-01-06 12:33:12 +1300

    passed matrix test

--- matrix.js
+++ matrix.js
@@ -1,5 +1,34 @@
 function getMatrix (n) {
+  let newArr = []
+  for (let i = 0; i < n; i++) {
+    newArr.push([])
+    for (let j = 0; j < n; j++) {
+      newArr[i].push(0)
+    }
+  }
+  return newArr
 }
 
 function updateMatrix (matrix, coords, value) {
+  let x = coords[0]
+  let y = coords[1]
+  // let newArr = []
+  for (let i = 0; i < matrix.length; i++) {
+    // newArr.push([])
+    for (let j = 0; j < matrix.length; j++) {
+      // newArr[i].push(0)
+      if (x === i && y === j) {
+        matrix[i].splice(j, i, value)
+        // newArr[i].pop()
+        // newArr[i].push(value)
+      }
+    }
+  }
+  // console.log(newArr)
+  return matrix
+}
+
+module.exports = {
+  getMatrix: getMatrix,
+  updateMatrix: updateMatrix
 }
