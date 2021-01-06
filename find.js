95e4f6b65e10913bc1d8efb8be27eb2cb2b674ac
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-01-06 14:12:06 +1300

    still atempting find test

--- find.js
+++ find.js
@@ -1,2 +1,10 @@
 function find (arr, searchDetails) {
+    console.log(arr.find(contacts => searchDetails))
+    return arr.find(contacts => {
+        if(contacts ) {
+            return true
+        }
+    })
 }
+
+module.exports = find
